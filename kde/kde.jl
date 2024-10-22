9a07f284acb452ba7aed71e35d34f2e716ff4fd5
DeepWater3104 <redcurry1106@gmail.com>
2024-10-22 14:13:57 +0900

    update

--- kde/kde.jl
+++ kde/kde.jl
@@ -1,4 +1,5 @@
 using KernelDensity
+using Printf
 
 SpikeTime_typed = convert(Array{Float64,1}, SpikeTime)
 U = kde(SpikeTime_typed,bandwidth=10)
@@ -8,3 +9,11 @@ p2 = histogram(SpikeTime, bins=(range(0,1000,step=10)), xlims=(0,1000), label=""
 p3 = plot(U.x, U.density, xlims=(0, 1000), label="", title="kernel density", xlabel="Time[ms]")
 plot(p1, p2, p3, layout=(3,1), size=(900, 600))
 savefig("figure/result.png")
+
+# integration
+dt = U.x[2] - U.x[1]
+area = 0
+for i=1:length(U.x)
+    global area = area + U.density[i]*dt
+end
+@printf("area:%f", area)
